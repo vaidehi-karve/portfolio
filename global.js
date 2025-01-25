ef12b714876ed0a2de3c2d336edf82bef1ae5126
Vaidehi Karve <vkarve@ucsd.edu>
2025-01-24 16:52:04 -0800

    lab 3 final

--- global.js
+++ global.js
@@ -3,3 +3,69 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+// // Step 2
+// let navLink = $$("nav a")
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+//   currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'https://github.com/vaidehi-karve', title: 'Github' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav); 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    else if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+  }
+
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+            <option value=""></option>
+            <option value=lightdark>light dark</option>
+            <option value=light>light</option>
+            <option value=dark>dark</option>
+          </select>
+      </label>`
+);
+
+let select = document.querySelector(".color-scheme")
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
+  });
+
+if ("colorScheme" in localStorage) {
+    // ".color-scheme"===localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', localStorage.getItem('colorScheme'));
+    select.value = localStorage.getItem('colorScheme');
+}
