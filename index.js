1804971be516ab49b6a5cb33a65a3e7e2a5437e1
Vaidehi Karve <vkarve@ucsd.edu>
2025-01-30 17:26:37 -0800

    edited paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("testing testing testing")
-import { fetchJSON, renderProjects, fetchGitHubData } from '../global.js';
-const projects = await fetchJSON('lib/projects.json'); 
+import { fetchJSON, renderProjects, fetchGitHubData } from '../global.js';  
+const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
