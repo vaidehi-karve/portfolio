2df4de89660a59249707035857a2bfe0451cb3ee
Vaidehi Karve <vkarve@ucsd.edu>
2025-01-30 19:50:32 -0800

    changed paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("testing testing testing")
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';  
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
